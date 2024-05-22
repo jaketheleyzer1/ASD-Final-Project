0af4071c104cd2b6e4f090b8f2e46dbd32d9f32c
jaketheleyzer1 <jleyzerovich@ucvts.org>
2024-05-22 13:19:29 -0400

    rotation code

--- player.gd
+++ player.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 200
+const SPEED = 20
+const ROTATION_SPEED = 2.0  # Adjust this value for faster or slower rotation
 
-func _physics_process(_delta):
-	var direction = Vector2(0,0)
+func _physics_process(delta):
+	var direction = Vector2(0, 0)
+	
 	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
+		rotation -= ROTATION_SPEED * delta
 	if Input.is_action_pressed("ui_right"):
-		direction.x +=1
+		rotation += ROTATION_SPEED * delta
+	
 	if Input.is_action_pressed("ui_up"):
-		direction.y -=1
+		direction = Vector2(SPEED, 0).rotated(rotation)
 	if Input.is_action_pressed("ui_down"):
-		direction.y +=1
-	velocity = direction.normalized() * SPEED
+		direction = Vector2(-SPEED*0.8, 0).rotated(rotation)
+	
+	velocity = direction * SPEED
 	move_and_slide()
